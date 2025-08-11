4f80b51496e3885f3f04ea9d558371edb2859f30
DevilTai <53428521+TaiVSCode@users.noreply.github.com>
2025-08-11 16:37:10 +0800

    Update request.js

--- public/search/scripts/request.js
+++ public/search/scripts/request.js
@@ -2,6 +2,9 @@
 
 // Service definitions with dependency requirements
 const services = [
+	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.com/', action: 'select', requiresCodec: true },
+	{ name: 'AGE', icon: 'agedm', url: 'https://www.agedm.vip/', action: 'select', requiresCodec: true },
+	{ name: '樱花动漫', icon: 'yhdm', url: 'https://yhdm360.cc/', action: 'select', requiresCodec: true },
 	{ name: 'YouTube', icon: 'youtube', url: 'https://youtube.com/', action: 'select', requiresCodec: false },
 	{ name: 'TikTok', icon: 'tiktok', url: 'https://tiktok.com/', action: 'select', requiresCodec: true },
 	{ name: 'SoundCloud', icon: 'soundcloud', url: 'https://soundcloud.com/discover', action: 'select', requiresCodec: false },
@@ -10,12 +13,11 @@ const services = [
 	{ name: 'Rumble', icon: 'rumble', url: 'https://rumble.com/', action: 'select', requiresCodec: true },
 	{ name: 'DLive', icon: 'dlive', url: 'https://dlive.tv/', action: 'select', requiresCodec: true },
 	{ name: 'Kick', icon: 'kick', url: 'https://kick.com/', action: 'select', requiresCodec: true },
-	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.tv/', action: 'open', requiresCodec: true },
 	{ name: 'Archive', icon: 'archive', url: 'https://archive.org/details/movies', action: 'select', requiresCodec: true },
 	{ name: 'VKontakte', icon: 'vk', url: 'https://vk.com/video', action: 'select', requiresCodec: true },
 	{ name: 'Sibnet', icon: 'sibnet', url: 'https://video.sibnet.ru/rub/anime/', action: 'select', requiresCodec: true },
 	{ name: 'OK', icon: 'ok', url: 'https://ok.ru/video', action: 'select', requiresCodec: true },
-	{ name: 'Rutube', icon: 'rutube', url: 'https://rutube.ru/', action: 'open', requiresCodec: true }
+	{ name: 'Rutube', icon: 'rutube', url: 'https://rutube.ru/', action: 'select', requiresCodec: true }
 ];
 
 // Simplified codec checking (removed x86-64 requirement)
