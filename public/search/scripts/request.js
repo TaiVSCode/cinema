d2af08720af837e3c592aea446d7c2edce9c993b
DevilTai <53428521+TaiVSCode@users.noreply.github.com>
2025-09-13 06:05:24 +0800

    Update request.js

--- public/search/scripts/request.js
+++ public/search/scripts/request.js
@@ -3,7 +3,7 @@
 // Service definitions with codec requirements
 const services = [
 	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.com/', action: 'select', requiresCodec: true },
-	{ name: 'AGE', icon: 'agedm', url: 'https://www.agedm.vip/', action: 'select', requiresCodec: true },
+	{ name: 'AGE', icon: 'agedm', url: 'https://www.agedm.io/', action: 'select', requiresCodec: true },
 	{ name: '樱花动漫', icon: 'yhdm', url: 'https://ifana.cc/', action: 'select', requiresCodec: true },
 	{ name: 'YouTube', icon: 'youtube', url: 'https://youtube.com/', action: 'select', requiresCodec: false },
 	{ name: 'TikTok', icon: 'tiktok', url: 'https://tiktok.com/', action: 'select', requiresCodec: true },
