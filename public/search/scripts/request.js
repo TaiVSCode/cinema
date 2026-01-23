0c9233f742443f2d985b924b4182618ef08a9755
DevilTai <53428521+TaiVSCode@users.noreply.github.com>
2026-01-24 00:34:40 +0800

    Update request.js

--- public/search/scripts/request.js
+++ public/search/scripts/request.js
@@ -4,6 +4,7 @@
 const services = [
 	{ name: 'Bilibili', icon: 'bilibili', url: 'https://www.bilibili.com/', action: 'select', requiresCodec: true },
 	{ name: '嗷呜动漫', icon: 'aowu', url: 'https://www.aowu.tv/', action: 'select', requiresCodec: true },
+	{ name: 'CNFLIX', icon: 'cnflix', url: 'https://www.cnflix.tv/', action: 'select', requiresCodec: true },
 	{ name: 'AGE', icon: 'agedm', url: 'https://www.agedm.io/', action: 'select', requiresCodec: true },
 	{ name: '樱花动漫', icon: 'yhdm', url: 'https://ifana.cc/', action: 'select', requiresCodec: true },
 	{ name: 'YouTube', icon: 'youtube', url: 'https://youtube.com/', action: 'select', requiresCodec: false },
